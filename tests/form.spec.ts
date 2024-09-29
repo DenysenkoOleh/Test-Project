bd4ebb2f0a4ceae4e1642dcf0cf9500f98b937f8
oleh <oleh.denysenko@outlook.com>
2024-09-29 17:08:57 +0300

    eslint configuration

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -2,20 +2,20 @@ import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
 test.describe('Form', () => {
-    test('should submit form', async ({page}) => {
+    test('should submit form', async ({ page }) => {
         await page.goto('https://demoqa.com/automation-practice-form/');
 
         const firstName = faker.person.firstName();
         const lastName = faker.person.lastName();
         const email = faker.internet.email();
-        const phoneNumber = faker.string.numeric(10); 
-    
+        const phoneNumber = faker.string.numeric(10);
+
         await page.getByPlaceholder('First Name').fill(firstName);
         await page.getByPlaceholder('Last Name').fill(lastName);
         await page.getByPlaceholder('name@example.com').fill(email);
         // HACK: label intercepts the click event
         await page.getByText('Other').click();
-        await expect(page.getByRole('radio', {name: 'Other'})).toBeChecked();
+        await expect(page.getByRole('radio', { name: 'Other' })).toBeChecked();
         // await page.click('#gender-radio-3');
         await page.getByPlaceholder('Mobile Number').fill(phoneNumber);
 
