d4e09dcf6f259c6ef129095adb1113defb715061
oleh <oleh.denysenko@outlook.com>
2024-09-29 21:01:38 +0300

    Refactor form test to use generated form data

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -1,26 +1,81 @@
-import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+import { test, expect } from "@playwright/test";
+import { generateFormData } from "./generate-form-data";
 
-test.describe('Form', () => {
-    test('should submit only required fields', async ({ page }) => {
-        await page.goto('https://demoqa.com/automation-practice-form/');
+test.describe("Form", () => {
+  const formUrl = "https://demoqa.com/automation-practice-form/";
 
-        const firstName = faker.person.firstName();
-        const lastName = faker.person.lastName();
-        const email = faker.internet.email();
-        const phoneNumber = faker.string.numeric(10);
+  test("should submit only required fields", async ({ page }) => {
+    await page.goto(formUrl);
 
-        await page.getByPlaceholder('First Name').fill(firstName);
-        await page.getByPlaceholder('Last Name').fill(lastName);
-        await page.getByPlaceholder('name@example.com').fill(email);
-        // HACK: label intercepts the click event
-        await page.getByText('Other').click();
-        await expect(page.getByRole('radio', { name: 'Other' })).toBeChecked();
-        await page.getByPlaceholder('Mobile Number').fill(phoneNumber);
+    const data = generateFormData();
 
-        await page.getByRole('button', { name: 'Submit' }).click();
+    await page.getByPlaceholder("First Name").fill(data.firstName);
+    await page.getByPlaceholder("Last Name").fill(data.lastName);
+    await page.getByPlaceholder("name@example.com").fill(data.email);
+    // HACK: label intercepts the click event
+    await page.getByText("Other").click();
+    await expect(page.getByRole("radio", { name: "Other" })).toBeChecked();
+    await page.getByPlaceholder("Mobile Number").fill(data.phoneNumber);
 
-        await expect(page.getByText('Thanks for submitting the form')).toBeVisible();
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    await expect(
+      page.getByText("Thanks for submitting the form")
+    ).toBeVisible();
+  });
+  test("should submit all fields", async ({ page }) => {
+    await page.goto(formUrl);
+
+    const data = generateFormData();
+
+    await page.getByPlaceholder("First Name").fill(data.firstName);
+    await page.getByPlaceholder("Last Name").fill(data.lastName);
+    await page.getByPlaceholder("name@example.com").fill(data.email);
+
+    // HACK: label intercepts the click event
+    await page.getByText(data.gender, { exact: true }).click();
+    await expect(
+      page.getByRole("radio", { name: data.gender, exact: true })
+    ).toBeChecked();
+
+    await page.getByPlaceholder("Mobile Number").fill(data.phoneNumber);
+    await page.locator("#dateOfBirthInput").fill(data.formattedDateOfBirth);
+
+    await page.getByText(data.hobbies).click();
+    await expect(
+      page.getByRole("checkbox", { name: data.hobbies })
+    ).toBeChecked();
+
+    // TODO: pill input with dropdown
+    for (const subject of data.subjects) {
+      await page.locator("#subjectsInput").fill(subject);
+      await page.locator("#subjectsInput").press("Enter");
+    }
+    await page.getByLabel("Select picture").setInputFiles({
+      name: "file.png",
+      mimeType: "image/png",
+      buffer: Buffer.from("this is test"),
     });
+    await page.getByPlaceholder("Current Address").fill(data.address);
 
+    // Custom dropdown
+    const stateInput = page
+      .locator("#stateCity-wrapper")
+      .locator("input")
+      .first();
+    await stateInput.fill(data.state);
+    await stateInput.press("Enter");
+
+    const cityInput = page
+      .locator("#stateCity-wrapper")
+      .locator("input")
+      .last();
+    await cityInput.fill(data.city);
+    await cityInput.press("Enter");
+
+    await page.getByRole("button", { name: "Submit" }).click();
+    await expect(
+      page.getByText("Thanks for submitting the form")
+    ).toBeVisible();
+  });
 });
---
+++ tests/generate-form-data.ts
@@ -0,0 +1,61 @@
+import { faker } from '@faker-js/faker';
+import { format } from 'date-fns';
+
+const STATE_CITY_MAP = {
+    'NCR': ['Delhi', 'Gurgaon', 'Noida'],
+    'Uttar Pradesh': ['Agra', 'Merrut', 'Lucknow'],
+    'Haryana': ['Karnal', 'Panipat'],
+    'Rajasthan': ['Jaipur', 'Jaisalmer']
+};
+
+const SUBJECTS = [
+    'Hindi',
+    'English',
+    'Maths',
+    'Physics',
+    'Chemistry',
+    'Biology',
+    'Computer Science',
+    'Commerce',
+    'Accounting',
+    'Economics',
+    'Arts',
+    'Social Studies',
+    'History',
+    'Civics'
+];
+
+const HOBBIES = [
+    'Sports',
+    'Reading',
+    'Music'
+]
+
+const GENDERS = [
+    'Male',
+    'Female',
+    'Other'
+]
+
+
+
+export function generateFormData() {
+    const dateOfBirth = faker.date.past();
+    const state = faker.helpers.objectKey(STATE_CITY_MAP);
+    const city = faker.helpers.arrayElement(STATE_CITY_MAP[state]);
+
+    return {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        gender: faker.helpers.arrayElement(GENDERS),
+        subjects: faker.helpers.arrayElements(SUBJECTS, 3),
+        phoneNumber: faker.string.numeric(10),
+        dateOfBirth: dateOfBirth,
+        formattedDateOfBirth: format(dateOfBirth, 'dd MMM yyyy'),
+        hobbies: faker.helpers.arrayElement(HOBBIES),
+        address: faker.location.streetAddress(),
+        state: state,
+        city: city
+    };
+}
